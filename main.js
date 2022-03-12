bd17afa7a917288f7a99cbe814146dbffc170530
oscargarciaporras <osligarpo92@gmail.com>
2022-03-12 11:04:08 -0500

    ciclo for in y for of

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 
 
-for (let i = 0; i < 5; i++) {
-    console.log(`Ciclo normal ${i}`);
+let data = new Array(5).fill(null);
+Object.preventExtensions(data);
+data[4] = "Miguel";
+console.log(data);
+
+console.log("Indice");
+for(let i in data){
+    console.log(`${i}.`);
+}
+console.log("Valor");
+for(let v of data){
+    console.log(`${v}`);
+}
+console.log("Indice y Valor");
+for(let [indice,valor] of Object.entries(data)){
+    console.log(`${indice} = ${valor}`);
 }
 
-
-let x=0;
-for(x; true;){
-    if(x<5){
-        console.log(`Ciclo personalizado ${x}`);
-        x++;
-    }else{
-        break;
-    }
-}
