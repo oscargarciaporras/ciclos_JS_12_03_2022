0b8fde823d385905f4850395b497b714fdd9229c
oscargarciaporras <osligarpo92@gmail.com>
2022-03-17 20:01:47 -0500

    ciclo map mosificar resultados

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 
-let data = new Array(5).fill(null);
-Object.preventExtensions(data);
-data.fill("Miguel", 4);
+// let data = new Array(5).fill(null);
+// Object.preventExtensions(data);
+// data.fill("Miguel", 4);
+function saldar() {
+    return "Hola Como estas";
+}
+let data = {
+    nombre: "Miguel",
+    apellido : "Castro",
+    edad: 23,
+    saldar
+}
 
-
-let datos = data.map(function(v,i){
-    if(v == null){
-        return i+2;
+let datos = Object.entries(data).map(function(elemt){
+    let obj = {};
+    if(typeof(elemt[1]) == "function"){
+        obj.saludar = data.saldar();
     }else{
-        return v;
+        if(elemt[0] == "nombre"){
+            obj[elemt[0]] = `${elemt[1].toUpperCase()} Angel`;
+        }else{
+            obj[elemt[0]] = elemt[1];
+        }
     }
+    return obj;
 })
 
 console.log(data);
-console.log(datos);
+console.log(Object.assign({}, ...datos) );
+
+
 
