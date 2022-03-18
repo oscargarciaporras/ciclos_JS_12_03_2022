a6e175db53dd2d08919633d404ce703d70de5138
oscargarciaporras <osligarpo92@gmail.com>
2022-03-17 21:24:36 -0500

    propiedades, break, continue, label

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+multiplicando:
+for (let i = 1; i <= 50; i++) {
+    console.log(`%cTabla del ${i}`,"color:red;");
+    multiplicador:
+    for (let x = 1; x <= 10; x++) {
+        let resultado = i*x;
+        if(resultado%2==0){
+            console.log(`${i} x ${x} = Numero par`);
+            continue multiplicador;
+        }
+        console.log(`${i} x ${x} = ${resultado}`);
+    }
+}
 
-var papitas = 10;
-do{
-    console.log(`Me comi ${papitas} papitas`);
-    papitas--;
-}while(false)
 
-do{
-    console.log(`Me comi ${papitas} papitas`);
-    papitas--;
-}while(papitas)
