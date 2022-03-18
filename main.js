16577b179fa403bfec0a432f9cd106fe659bedce
oscargarciaporras <osligarpo92@gmail.com>
2022-03-17 20:42:50 -0500

    ciclo do While

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 
-let papitas = 10;
-
-console.group("Ciclo 1");
-while(papitas){
+var papitas = 10;
+do{
     console.log(`Me comi ${papitas} papitas`);
     papitas--;
-}
-console.groupEnd();
-console.group("Ciclo 2");
-while(papitas <= 10){
+}while(false)
+
+do{
     console.log(`Me comi ${papitas} papitas`);
-    papitas++;
-}
-console.groupEnd();
+    papitas--;
+}while(papitas)
