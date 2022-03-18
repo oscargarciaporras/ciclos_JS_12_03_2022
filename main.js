888fa1107f6131b0f1061feb9be3d37055cd314b
oscargarciaporras <osligarpo92@gmail.com>
2022-03-17 20:35:14 -0500

    ciclo While

--- main.js
+++ main.js
@@ -1,33 +1,15 @@
 
-// let data = new Array(5).fill(null);
-// Object.preventExtensions(data);
-// data.fill("Miguel", 4);
-function saldar() {
-    return "Hola Como estas";
+let papitas = 10;
+
+console.group("Ciclo 1");
+while(papitas){
+    console.log(`Me comi ${papitas} papitas`);
+    papitas--;
 }
-let data = {
-    nombre: "Miguel",
-    apellido : "Castro",
-    edad: 23,
-    saldar
+console.groupEnd();
+console.group("Ciclo 2");
+while(papitas <= 10){
+    console.log(`Me comi ${papitas} papitas`);
+    papitas++;
 }
-
-let datos = Object.entries(data).map(function(elemt){
-    let obj = {};
-    if(typeof(elemt[1]) == "function"){
-        obj.saludar = data.saldar();
-    }else{
-        if(elemt[0] == "nombre"){
-            obj[elemt[0]] = `${elemt[1].toUpperCase()} Angel`;
-        }else{
-            obj[elemt[0]] = elemt[1];
-        }
-    }
-    return obj;
-})
-
-console.log(data);
-console.log(Object.assign({}, ...datos) );
-
-
-
+console.groupEnd();
